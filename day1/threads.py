0b059095ab45e97f7a6655b9dbc73bd9cf67cedf
Vinicius-O-Ferraz <vinicius.f.ferraz.bsi@gmail.com>
2024-08-12 21:17:15 -0300

    minor

---
+++ day1/threads.py
@@ -0,0 +1,20 @@
+import threading
+import time
+
+
+done = False
+
+def worker():
+    counter = 0
+    while not done:
+        time.sleep(1)
+        counter += 1
+        print(counter)
+
+threading.Thread(target=worker, daemon = True).start()
+'''
+    pass a function as a thread
+'''
+
+input('Press enter to quit: ')
+done = True
